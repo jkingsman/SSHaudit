5431fa71c4dbce4f5e72e1763e53c1e103f2323c
Jack Kingsman <jack.kingsman@gmail.com>
2015-09-17 11:39:42 -0700

    Fix spacing on kmap

--- index.js
+++ index.js
@@ -94,14 +94,14 @@ function printList() {
                 userList.hasOwnProperty(user) && userList[user].isFlagged)) {
             /* K-map reduction of this:
              *      A	B	C	Y
-             *    0	0	0	0	0
-             *    1	0	0	1	0
-             *    2	0	1	0	0
-             *    3	0	1	1	0
-             *    4	1	0	0	1
-             *    5	1	0	1	0
-             *    6	1	1	0	1
-             *    7	1	1	1	1
+             *      0	0	0	0
+             *    	0	0	1	0
+             *    	0	1	0	0
+             *    	0	1	1	0
+             *    	1	0	0	1
+             *    	1	0	1	0
+             *    	1	1	0	1
+             *    	1	1	1	1
              *  where A = hasOwnProperty, B = userList[user].isFlagged,
              *  C = program.flaggedonly, Y = should show entry
              */
